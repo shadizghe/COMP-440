4ab80b93119680989aa1b6fc9d345626b80e0f13
SpencerCrowell <spencer.crowell.694@my.csun.edu>
2025-04-17 15:15:25 -0700

    working search function

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def connect_db():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="$root",  # Change this to your MySQL password
+        password="$Beanman069",  # Change this to your MySQL password
         database="projectdb"
     )
 
--- sql/create_rental_unit.sql
+++ sql/create_rental_unit.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS rental_unit (
+CREATE TABLE rental_unit (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     title VARCHAR(255) NOT NULL,
     details TEXT,
-    features VARCHAR(255),  -- can be a comma-separated list for now
+    features VARCHAR(255),
     price DECIMAL(10, 2) NOT NULL,
     posted_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     
